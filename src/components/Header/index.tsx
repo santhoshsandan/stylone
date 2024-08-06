b8565f9732997f71c65a33d9bd421f546baf5ef9
Santhosh <116790255+santhoshsandan@users.noreply.github.com>
2024-08-06 16:01:52 +0530

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -147,7 +147,7 @@ const Header = () => {
               </div>
               <div className="flex items-center justify-end pr-16 lg:pr-0">
                 <Link
-                  href="https://project.pruset.com/"
+                  href="https://project.pruset.com/login"
                   className="hidden px-7 py-3 text-base font-medium text-dark hover:opacity-70 dark:text-white md:block"
                 >
                   Customer Support
